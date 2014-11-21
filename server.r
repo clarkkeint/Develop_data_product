3a2b807f7f1b39550e86153a8ba76cd6d37b9f50
clarkkeint <liqisui@gmail.com>
2014-11-21 22:18:00 +0100

    Programe code

--- server.r
+++ server.r
@@ -1,10 +1,13 @@
 library(UsingR)
-data(galton)
+data(galton) # Use Galton's data
+
+# Creat a predicition function to predict chlid's height
 predictfunction <- function(invalue){fit <- lm(child ~ parent,data=galton)
                                     predict(fit, newdata = data.frame(parent=c(invalue)))}
 
 shinyServer(
   function(input, output) {
+      # Plot data points with frequancy. Big size points correspond more data
       output$newSizePoint  <- renderPlot(width = 450, height = 450,{
       freqData <- as.data.frame(table(galton$child, galton$parent))
       names(freqData) <- c("child", "parent", "freq")
@@ -13,15 +16,18 @@ shinyServer(
            pch = 21, col = "black", bg = "lightblue",
            cex = .15 * freqData$freq, 
            xlab = "parent", ylab = "child")
+      # Creat a regression model
       fit <- lm(child ~ parent,data=galton)
       lines(galton$parent,fit$fitted,col="red",lwd=3)
+      # Plot the mean value
       points(mean(galton$parent), mean(galton$child), col='red',cex = 2, pch = 19)
+      # Plot prediction value
       prediction <- predict(fit, newdata = data.frame(parent=c(input$parent)))
       points(input$parent, prediction, col='blue',cex = 2, pch = 19)      
       lines(c(input$parent,input$parent),c(60,prediction),col="blue",lwd=1)
       lines(c(60,input$parent),c(prediction,prediction),col="blue",lwd=1)
       }) 
-      
+      # Output the initial value and prediction value
       output$value <- renderPrint({input$parent})
       output$child <- renderPrint({predictfunction(input$parent)[[1]]})
   }
--- ui.R
+++ ui.R
@@ -1,8 +1,11 @@
+# Define UI for application that predict child's height from parents' height
 shinyUI(pageWithSidebar(
   headerPanel("Predict children's height by parents'  height"),  
   sidebarPanel(
     h4('Use the bar below to input parent\'s height'),
+    # Input parents' height
     sliderInput('parent', 'Parent\'s height (inche)',value = 66, min = 62, max = 74, step = 0.05,),
+    # Give some introduction
     h4('Introduction:'),
     h5('* This App is used to predict child\'s height from parents\' height.'), 
     h5('* The data set are from Galton\'s height data for parents and children in 1885.'),
@@ -12,10 +15,13 @@ shinyUI(pageWithSidebar(
     h5('* The blue point are pair of prediction of child\'s height and inputed parent\'s height.')
     ),
   mainPanel(
+    # Show the input value
     h4('You entered Parent\'s height is : (inche)'),
     verbatimTextOutput("value"),
+    # Show predict value
     h4('Prediction of children\'s height is : (inche)'),
     verbatimTextOutput("child"), 
+    # Plot a regression line and data points
     h4('Fitted line'),
     plotOutput('newSizePoint')
     
